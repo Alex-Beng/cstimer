5aae992833cb4207ccd071de8cd023b5b8fb0b6e
Alex Beng <599267065@qq.com>
2025-01-30 10:41:49 +0800

    fix timer

--- src/js/stats/stats.js
+++ src/js/stats/stats.js
@@ -54,7 +54,7 @@ var stats = execMain(function(kpretty, round, kpround) {
 		} else {
 			console.log('new time = ', time)
 			var faster_than_last = (times.length > 1) ? (timeAt[times.length-1][1] >= time[1]) : false;
-			times.push([time, curScramble, "", Math.round((new Date().getTime() - time[1]) / 1000)], [], faster_than_last);
+			times.push([time, curScramble, "", Math.round((new Date().getTime() - time[1]) / 1000), [], faster_than_last]);
 		}
 		timesExtra.push(null);
 		times_stats_table.pushed();
