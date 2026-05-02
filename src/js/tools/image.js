06731ef2ae7227e2e64e4b1664812395f9b0fb77
Alex Beng <599267065@qq.com>
2026-05-02 16:29:04 +0800

    fix clock

--- src/js/tools/image.js
+++ src/js/tools/image.js
@@ -1109,7 +1109,7 @@ var image = (function() {
 			status = nnnImage.genPosit(scramble[2], scramble[1]);
 			type = `${scramble[2]}${scramble[2]}${scramble[1]}`
 		} else if (type == "clk") {
-			status = clkImage.genPosit(scramble[1]);
+			status = clkImage.genPosit(scramble[1])[1];
 		}
 
 		if (typeof status !== "undefined") {
