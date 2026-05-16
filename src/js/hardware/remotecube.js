92bd5f5eda235ba1ab2d7176b24b3de4b60e5daa
Alex Beng <599267065@qq.com>
2026-05-17 01:35:21 +0800

    webserial api& queue

--- src/js/hardware/remotecube.js
+++ src/js/hardware/remotecube.js
@@ -2,6 +2,11 @@
 
 var RemoteCube = (function() {
 	var baseUrl = '';
+	var port = null;
+	var writer = null;
+	var useSerial = localStorage['remoteCubeMode'] == 'serial';
+	var sendQueue = [];
+	var sending = false;
 
 	function connect(url, callback) {
 		baseUrl = url.replace(/\/$/, '');
@@ -20,21 +25,95 @@ var RemoteCube = (function() {
 	}
 
 	function sendMoves(moves) {
-		if (!baseUrl || moves.length == 0) return;
-		$.ajax({
-			url: baseUrl + '/api/moves',
-			type: 'POST',
-			contentType: 'application/json',
-			data: JSON.stringify(moves),
-			timeout: 5000,
-			error: function(xhr, status, err) {
-				DEBUG && console.log('[remotecube] sendMoves failed:', err || status);
-			}
+		if (moves.length == 0) return;
+		sendQueue.push(moves);
+		drainQueue();
+	}
+
+	function drainQueue() {
+		if (sending || sendQueue.length == 0) return;
+		sending = true;
+		var moves = sendQueue.shift();
+		if (useSerial && writer) {
+			var cmd = JSON.stringify(moves) + '\n';
+			var encoder = new TextEncoder();
+			writer.write(encoder.encode(cmd)).then(function() {
+				sending = false;
+				drainQueue();
+			}).catch(function(err) {
+				DEBUG && console.log('[remotecube] serial send failed:', err);
+				sending = false;
+				drainQueue();
+			});
+		} else if (baseUrl) {
+			$.ajax({
+				url: baseUrl + '/api/moves',
+				type: 'POST',
+				contentType: 'application/json',
+				data: JSON.stringify(moves),
+				timeout: 5000,
+				complete: function() {
+					sending = false;
+					drainQueue();
+				},
+				error: function(xhr, status, err) {
+					DEBUG && console.log('[remotecube] sendMoves failed:', err || status);
+				}
+			});
+		} else {
+			sending = false;
+		}
+	}
+
+	function setMode(mode) {
+		if (mode == 'serial' && !port) {
+			return openSerial();
+		} else if (mode == 'http') {
+			disconnectSerial();
+			useSerial = false;
+			localStorage['remoteCubeMode'] = 'http';
+			return Promise.resolve('http');
+		}
+		return Promise.resolve(useSerial ? 'serial' : 'http');
+	}
+
+	function openSerial() {
+		if (!('serial' in navigator)) {
+			return Promise.reject('Web Serial API 不可用');
+		}
+		return navigator.serial.requestPort().then(function(p) {
+			port = p;
+			return port.open({ baudRate: 115200 });
+		}).then(function() {
+			writer = port.writable.getWriter();
+			useSerial = true;
+			localStorage['remoteCubeMode'] = 'serial';
+			return 'serial';
+		}).catch(function(err) {
+			DEBUG && console.log('[remotecube] serial open failed:', err);
+			throw err;
 		});
+	}
+
+	function disconnectSerial() {
+		if (writer) {
+			writer.releaseLock();
+			writer = null;
+		}
+		if (port) {
+			port.close();
+			port = null;
+		}
+	}
+
+	function getMode() {
+		return useSerial ? 'serial' : 'http';
 	}
 
 	return {
 		connect: connect,
-		sendMoves: sendMoves
+		sendMoves: sendMoves,
+		setMode: setMode,
+		getMode: getMode
 	};
 })();
--- src/js/tools/remotecube.js
+++ src/js/tools/remotecube.js
@@ -5,15 +5,22 @@ var remotecubeUtil = execMain(function() {
 	var urlInput = $('<input type="text" style="width:100%">').val(localStorage['remoteCubeUrl'] || '');
 	var statusDiv = $('<span>');
 	var saveBtn = $('<input type="button" class="buttonOK" value="保存">');
+	var serialBtn = $('<input type="button" class="buttonOK" value="连接串口">');
+	var modeSpan = $('<span>');
 
 	function execFunc(fdiv) {
 		if (!fdiv) return;
 		urlInput.val(localStorage['remoteCubeUrl'] || '');
+		refreshMode();
 		fdiv.empty().append(
 			$('<p>').text('远程魔方地址:'),
 			urlInput,
 			'<br><br>',
 			saveBtn,
+			'<br><br>',
+			$('<p>').text('下发方式:'),
+			modeSpan,
+			serialBtn,
 			'<br><br>',
 			statusDiv
 		);
@@ -24,6 +31,22 @@ var remotecubeUtil = execMain(function() {
 			statusDiv.text('已保存').css('color', '#0f0');
 			setTimeout(function() { statusDiv.text(''); }, 2000);
 		});
+		serialBtn.unbind('click').click(function() {
+			$.waitUser.call();
+			if (RemoteCube.getMode() == 'serial') {
+				RemoteCube.setMode('http').then(refreshMode);
+			} else {
+				RemoteCube.setMode('serial').then(refreshMode, function(err) {
+					statusDiv.text('串口连接失败: ' + err).css('color', '#f00');
+				});
+			}
+		});
+	}
+
+	function refreshMode() {
+		var mode = RemoteCube.getMode();
+		modeSpan.text('当前: ' + (mode == 'serial' ? '串口' : 'HTTP'));
+		serialBtn.val(mode == 'serial' ? '切换到 HTTP' : '连接串口');
 	}
 
 	$(function() {
