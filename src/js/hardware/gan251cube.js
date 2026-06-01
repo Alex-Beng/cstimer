0c4f989990c25881093139c35fc608fb511bb788
Alex Beng <599267065@qq.com>
2026-06-01 16:48:18 +0800

    add log

--- src/js/hardware/gan251cube.js
+++ src/js/hardware/gan251cube.js
@@ -296,8 +296,12 @@ execMain(function() {
 			return;
 		}
 
+		var rawHex = data.map(function(b) { return ('0' + b.toString(16)).slice(-2); }).join(' ');
+		giikerutil.log('[gan251cube] raw:', rawHex);
 		var decrypted = decryptPacket(data, decoder.key, decoder.iv);
 		decrypted = trimTrailingZeros(decrypted);
+		var decHex = decrypted.map(function(b) { return ('0' + b.toString(16)).slice(-2); }).join(' ');
+		giikerutil.log('[gan251cube] dec:', decHex);
 		processDecryptedPacket(decrypted);
 	}
 
@@ -328,6 +332,8 @@ execMain(function() {
 		giikerutil.log('[gan251cube] MAC:', deviceMac);
 		var keyIv = deriveKeyIv(deviceMac);
 		if (!keyIv) return false;
+		giikerutil.log('[gan251cube] KEY:', keyIv.key.join(','));
+		giikerutil.log('[gan251cube] IV :', keyIv.iv.join(','));
 		decoder = $.aes128(keyIv.key);
 		decoder.iv = keyIv.iv;
 		decoder.key = keyIv.key;
