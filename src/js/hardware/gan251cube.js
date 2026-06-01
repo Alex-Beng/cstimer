736da96d102932cc19938a9efaf0a72269e90a64
Alex Beng <599267065@qq.com>
2026-06-01 14:35:22 +0800

    add debug log

--- src/js/hardware/gan251cube.js
+++ src/js/hardware/gan251cube.js
@@ -115,6 +115,7 @@ execMain(function() {
 
 	function validateCrc16(decrypted) {
 		if (decrypted.length < 3) {
+			giikerutil.log('[gan251cube] CRC skip: too short, len=' + decrypted.length);
 			return false;
 		}
 		var body = decrypted.slice(1, -2);
@@ -123,10 +124,12 @@ execMain(function() {
 		var expected_le = crcLo | (crcHi << 8);
 		var expected_be = crcHi | (crcLo << 8);
 		var computed = crc16CcittFalse(body);
+		giikerutil.log('[gan251cube] CRC debug: body(no-id) ' + body.join(',') + ' crcHi=' + crcHi.toString(16) + ' crcLo=' + crcLo.toString(16) + ' exp_le=' + expected_le.toString(16) + ' exp_be=' + expected_be.toString(16) + ' computed=' + computed.toString(16));
 		if (computed === expected_le) { giikerutil.log('[gan251cube] CRC OK: body(no-id) LE'); return true; }
 		if (computed === expected_be) { giikerutil.log('[gan251cube] CRC OK: body(no-id) BE'); return true; }
 		var bodyWithId = decrypted.slice(0, -2);
 		computed = crc16CcittFalse(bodyWithId);
+		giikerutil.log('[gan251cube] CRC debug: body(+id)  ' + bodyWithId.join(',') + ' computed(+id)=' + computed.toString(16));
 		if (computed === expected_le) { giikerutil.log('[gan251cube] CRC OK: body(+id) LE'); return true; }
 		if (computed === expected_be) { giikerutil.log('[gan251cube] CRC OK: body(+id) BE'); return true; }
 		return false;
@@ -300,7 +303,9 @@ execMain(function() {
 		}
 
 		var decrypted = decryptPacket(data, decoder.key, decoder.iv);
+		giikerutil.log('[gan251cube] raw=' + data.join(','));
 		decrypted = trimTrailingZeros(decrypted);
+		giikerutil.log('[gan251cube] dec=' + decrypted.join(','));
 		processDecryptedPacket(decrypted);
 	}
 
