ace5f23e0668e44b243d7190b9d2b030022f85f4
Alex Beng <599267065@qq.com>
2026-06-04 00:38:57 +0800

    add log

--- src/js/hardware/gan251cube.js
+++ src/js/hardware/gan251cube.js
@@ -307,6 +307,7 @@ execMain(function() {
 				var fl = f.join('');
 				giikerutil.log('[gan251cube] facelet:', fl);
 				cubie.fromFacelet(fl);
+				giikerutil.log('[gan251cube] facelet:2', cubie.toFaceCube());
 				GiikerCube.callback(cubie.toFaceCube(), [], [0, $.now()], deviceName);
 			}
 		} else if (packetId === 0xef) {
