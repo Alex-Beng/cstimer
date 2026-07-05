94d32ebb3f0b4cabf5493d88239558f0388437a6
Alex Beng <599267065@qq.com>
2026-07-05 21:49:46 +0800

    add log

--- src/js/timer/remotecube.js
+++ src/js/timer/remotecube.js
@@ -154,6 +154,7 @@ execMain(function(timer) {
 			var curProgress;
 			if (mstep == 2) {
 				curProgress = puzzleObj.isSolved(kernel.getProp('vrcMP', 'n'));
+				console.log('[rc] isSolved:', curProgress, 'method:', kernel.getProp('vrcMP', 'n'), 'puzzle:', getPuzzle());
 				timer.updateMulPhase(totPhases, curProgress, now);
 			}
 			if (mstep == 2 && curProgress == 0) {
